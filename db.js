d22d158482354790f7d2add5f77c6423cecf27e5
jlywashere <102553299+jlywashere@users.noreply.github.com>
2023-04-20 11:21:08 -0400

    DB connector for server

---
+++ db.js
@@ -0,0 +1,21 @@
+const db = require("mysql")
+require("dotenv").config()
+
+const dbConn = db.createConnection({
+	
+	host : process.env.DB_HOST,
+	user : process.env.DB_USER,
+	password : process.env.DB_PASSWORD,
+	database : process.env.DB_NAME
+
+})
+
+dbConn.connect(function(e){
+
+	if(!e){
+		console.log(`Connected to database: ${process.env.DB_NAME}`)
+	} else {
+		console.log(`Error connecting to database: ${process.env.DB_NAME}`) 
+	}
+
+})
