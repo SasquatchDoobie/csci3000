8874914c926fbb4d6be0da0e58afda2a906e6e72
jlywashere <102553299+jlywashere@users.noreply.github.com>
2023-04-24 21:17:51 -0400

    look mom i have no idea what im doing

---
+++ passport.js
@@ -0,0 +1,36 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getUserByUsername, getUserById) {
+
+	const authenticateUser = async (username, password, done) => {
+
+		const user = getUserByUsername(username)
+		
+		if (user == null) return done(null, false, { message: 'Username does not exist' })
+
+		try {
+			
+			if (await bcrypt.compare(password, user.password)) {
+			
+				return done(null, user)
+			
+			} else {
+				return done(null, false, { message: 'Password is incorrect' })
+			}
+
+		} catch (e) {
+			return done(e)
+		}
+	}
+
+	passport.use(new LocalStrategy({ usernameField: 'username' }, authenticateUser))
+	
+	passport.serializeUser((user, done) => done(null, user.id))
+	
+	passport.deserializeUser((id, done) => {
+		return done(null, getUserById(id))
+	})
+}
+
+module.exports = initialize
