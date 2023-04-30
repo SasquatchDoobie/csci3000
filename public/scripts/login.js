c24e03bf95dd5e08b55d9244dfeabce705da25b9
SasquatchDoobie <130501694+SasquatchDoobie@users.noreply.github.com>
2023-04-30 03:12:35 -0400

    Update login.js

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,9 +1,6 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
-
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -11,13 +8,4 @@ registerLink.addEventListener('click', ()=> {
 
 loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
-});
-
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-    wrapper.classList.add('active-popup');
-});
-
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
 });
