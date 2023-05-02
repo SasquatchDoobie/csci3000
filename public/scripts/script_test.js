fd3d1dfbac8487cc6c6f97fad6243a06cf804623
SasquatchDoobie <130501694+SasquatchDoobie@users.noreply.github.com>
2023-05-01 22:51:20 -0400

    added basic form validation for album form

--- public/scripts/script_test.js
+++ public/scripts/script_test.js
@@ -11,3 +11,22 @@ input.addEventListener("change", () => {
     console.log(files)
 
 })
+
+//========================
+// Album Form Validation
+//========================
+function validateForm() {
+    var albumname = document.getElementById("albumname").value;
+    var alphaNumeric = /^[0-9a-zA-Z\s]+$/;
+
+    // Checks for empty input, then checks for alpha numeric (allows spaces)
+    if (albumname.trim() === "") {
+      alert("Album name cannot be empty");
+      return false;
+    } else if (!albumname.match(alphaNumeric)) {
+      alert("Album name can only contain alphanumeric characters");
+      return false;
+    } else {
+      return true;
+    }
+}
